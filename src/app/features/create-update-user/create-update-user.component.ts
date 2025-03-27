fda7df09a77edbfd684f3186707f7d583bd82a53
malpaniriya <139846863+malpaniriya@users.noreply.github.com>
2025-03-27 13:12:36 +0530

    folders added in app

---
+++ src/app/features/create-update-user/create-update-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-user',
+  imports: [],
+  templateUrl: './create-update-user.component.html',
+  styleUrl: './create-update-user.component.scss'
+})
+export class CreateUpdateUserComponent {
+
+}
