d666511070e2cb67c9c2c9c7a36491131129f76b
malpaniriya <139846863+malpaniriya@users.noreply.github.com>
2025-03-27 14:26:38 +0530

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Features/login/login.component';
-import { LayoutComponent } from './Features/layout/layout.component';
-import { DashboardComponent } from './Features/dashboard/dashboard.component';
-import { UserListComponent } from './Features/user-list/user-list.component';
-import { UserDetailsComponent } from './Features/user-details/user-details.component';
-import { CreateUpdateUserComponent } from './Features/create-update-user/create-update-user.component';
+import { LoginComponent } from './features/login/login.component';
+import { LayoutComponent } from './features/layout/layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { UserListComponent } from './features/user-list/user-list.component';
+import { UserDetailsComponent } from './features/user-details/user-details.component';
+import { CreateUpdateUserComponent } from './features/create-update-user/create-update-user.component';
 
 export const routes: Routes = [
     
